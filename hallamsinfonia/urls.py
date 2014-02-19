2b328d040dc71a8ef6b33cfb36cd61b6a9406ab9
Blake Hemingway <blakehemingway@gmail.com>
2014-02-19 17:24:50 +0000

    Various Old Tweaks - Just Syncing

--- hallamsinfonia/urls.py
+++ hallamsinfonia/urls.py
@@ -5,7 +5,11 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 
-    url(r'^$', 'hallamsinfonia.views.static', name='static'),
+    url(r'^$', 'hallamsinfonia.views.home', name='home'),
+    url(r'^about/$', 'hallamsinfonia.views.about', name='about'),
+    url(r'^upcoming/$', 'hallamsinfonia.views.upcoming', name='upcoming'),
+    url(r'^news/$', 'hallamsinfonia.views.news', name='news'),
+    url(r'^conductors/$', 'hallamsinfonia.views.conductors', name='conductors'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
--- hallamsinfonia/views.py
+++ hallamsinfonia/views.py
@@ -1,5 +1,17 @@
 # Temporary view work on the front-end as a static page.
 from django.shortcuts import render
 
-def static(request):
-    return render(request, 'base_2.html')
+def home(request):
+    return render(request, 'home.html')
+    
+def upcoming(request):
+    return render(request, 'upcoming.html')
+    
+def news(request):
+    return render(request, 'news.html')
+    
+def about(request):
+    return render(request, 'about.html')
+    
+def conductors(request):
+    return render(request, 'conductors.html')
