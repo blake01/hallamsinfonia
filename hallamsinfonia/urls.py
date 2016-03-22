b8643354b0ab171f04a43db4cf53f620f8ff4dce
Blake Hemingway <blakehemingway@gmail.com>
2016-03-22 23:35:23 +0000

    Fix deprecation warnings

--- hallamsinfonia/urls.py
+++ hallamsinfonia/urls.py
@@ -1,26 +1,24 @@
 from django.conf.urls import patterns, include, url, static
 from django.conf import settings
-from hallamsinfonia.views import NewsListView
+from hallamsinfonia import views
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     # Main views
-    url(r'^$', 'hallamsinfonia.views.home', name='home'),
-    url(r'^about/$', 'hallamsinfonia.views.about', name='about'),
-    url(r'^upcoming/$', 'hallamsinfonia.views.upcoming', name='upcoming'),
-    url(r'^news/$', NewsListView.as_view(), name='news'),
-    url(r'^conductors/$', 'hallamsinfonia.views.conductors', name='conductors'),
+    url(r'^$', views.home, name='home'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^upcoming/$', views.upcoming, name='upcoming'),
+    url(r'^news/$', views.NewsListView.as_view(), name='news'),
+    url(r'^conductors/$', views.conductors, name='conductors'),
     # Ajax views
-    url(r'^conductors/(?P<pk>[\d]+)/$', 'hallamsinfonia.views.ajax_conductor',
+    url(r'^conductors/(?P<pk>[\d]+)/$', views.ajax_conductor,
         name='ajax_conductor'),
-    url(r'^upcoming/(?P<pk>[\d]+)/$', 'hallamsinfonia.views.ajax_concert',
-        name='ajax_concert'),
+    url(r'^upcoming/(?P<pk>[\d]+)/$', views.ajax_concert, name='ajax_concert'),
     # Admin
     url(r'^admin/', include(admin.site.urls)),
-)
+]
 
 urlpatterns += static.static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
